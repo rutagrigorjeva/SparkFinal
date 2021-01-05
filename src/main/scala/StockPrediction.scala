0509d76ae535a58d46fda855e85bec2018e6c356
Ruta Grigorjeva <ruta.grigorjeva@gmail.com>
2021-01-05 19:02:46 +0100

    Small updates

--- src/main/scala/StockPrediction.scala
+++ src/main/scala/StockPrediction.scala
@@ -9,7 +9,8 @@ import org.apache.spark.sql.types.IntegerType
 import scalaj.http.Http
 
 object StockPrediction extends App {
-  val companys = Map("AAPL" -> "Apple, Inc.",
+  val companys = Map(
+    "AAPL" -> "Apple, Inc.",
     "SBUX" -> "Starbucks, Inc.",
     "MSFT" -> "Microsoft, Inc.",
     "CSCO" -> "Cisco Systems, Inc.",
@@ -24,15 +25,19 @@ object StockPrediction extends App {
 
   val session = SparkSession.builder().appName("test").master("local").getOrCreate()
   println(s"Session started on Spark version ${session.version}")
+  // dfs is map of company shortcut to dataframe for that company
   val dfs = createDataFrames(companys)
 
   private def createDataFrames(companys: Map[String, String]): Map[String, DataFrame] = {
     companys.map(entry => {
       val companyShortcut = entry._1
+      println(companyShortcut)
       var df = session.read.format("csv")
         .option("header", "true")
         .load(s"./$companyShortcut.csv").toDF();
+      // remove surrounding white space from column names(trim)
       df = df.columns.foldLeft(df)((curr, n) => curr.withColumnRenamed(n, n.trim))
+      // USER DEFINED FUNCTION - process column cell data - removes dollar sign and cast column to double
       val removeDollarAndCastToDouble = udf((price: String) => {
         price.substring(2).toDouble
       })
@@ -48,20 +53,23 @@ object StockPrediction extends App {
       val avRet = df.select("Date", "average_return")
       avRet.show(truncate = false)
       avRet.write.mode("overwrite").parquet(s"$companyShortcut.parquet")
-      entry._1 -> df
+      companyShortcut -> df
     }).toSeq.toMap
   }
-  val frequnecy = dfs.map { case (name, df) => name -> df.agg("Frequency" -> "avg").collect()(0)(0).toString.toDouble }.toMap
-  println(frequnecy.maxBy { case (name, frequency) => frequency })
+  // frequency is a map of company shortcut name to avg frequency
+  val frequnecy = dfs.map { case (name, df) => name -> df.agg("Frequency" -> "avg")
+     .collect()(0)(0).toString.toDouble }.toMap
+  // this choose the company with max frequency
+  println("The most frequently traded stock is "+frequnecy.maxBy { case (name, frequency) => frequency })
 
   def downloadData(companys: Map[String, String]): Unit = {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     val currentDate = LocalDate.now();
     val dateEnd = formatter.format(currentDate);
     val dateStart = formatter.format(currentDate.minusYears(1));
-    val template = "https://www.nasdaq.com/api/v1/historical/{companyShortcut}/stocks/{dateStart}/{dateEnd}"
     for ((companyShortcut, companyFullName) <- companys) {
-      val url = template.replace("{companyShortcut}", URLEncoder.encode(companyShortcut, "UTF-8")).replace("{dateStart}", dateStart).replace("{dateEnd}", dateEnd)
+      val url = s"https://www.nasdaq.com/api/v1/historical/$companyShortcut/stocks/$dateStart/$dateEnd"
+      println(url)
       val result = Http(url).asString
       val body = result.body
       writeFile(companyShortcut + ".csv", body)
